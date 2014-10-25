c597dbdb697aeae1463224eef7ea91aee335987a
Manuel Dudda <manueldudda@redpeppix.de>
2014-10-26 00:04:19 +0200

    audio file

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 fc = $('#foreign_content').text();
 oc = $('#own_content').text();
-fc_words = fc.split(" ");
-oc_words = oc.split(" ");
+fc_words = fc.trim().split(" ");
+oc_words = oc.trim().split(" ");
 
 var interValId;
 
-function showNextWord()
-{
-  var nextFcWord = fc_words.shift();
-  var nextOcWord = oc_words.shift();
-  $('#foreign').html(nextFcWord);
-  $('#own').html(nextOcWord);
+function showNextWord() {
+    var nextFcWord = fc_words.shift();
+    var nextOcWord = oc_words.shift();
+    $('#foreign').html(nextFcWord);
+    $('#own').html(nextOcWord);
 
-  console.log("showNextWord");
-  if(fc_words.length == 0) {
-    window.clearInterval(interValId);
-  }
+    console.log("showNextWord");
+    if (fc_words.length == 0) {
+        window.clearInterval(interValId);
+    }
 }
 
+document.getElementById('audio').play();
 interValId = window.setInterval(showNextWord, 1000);
